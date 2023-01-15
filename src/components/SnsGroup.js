047f5d18ce0143a72756e9e9c8378950bee03d11
hoonsbory <tonymkcv@naver.com>
2023-01-15 22:33:55 +0900

    도메인 변경 site to ml

--- src/components/SnsGroup.js
+++ src/components/SnsGroup.js
@@ -1,12 +1,12 @@
-import React, { useEffect } from "react";
-import CopyURL from "../SnsIcon/CopyUrl";
-import FaceBook from "../SnsIcon/FaceBook";
-import Kakao from "../SnsIcon/Kakao";
-import NaverBlog from "../SnsIcon/NaverBlog";
-import LineDiv from "./LineDiv";
-import styled, { css } from "styled-components";
-import KakaoStory from "../SnsIcon/KakaoStory";
-import { Debounce } from "../Debounce";
+import React, { useEffect } from 'react';
+import CopyURL from '../SnsIcon/CopyUrl';
+import FaceBook from '../SnsIcon/FaceBook';
+import Kakao from '../SnsIcon/Kakao';
+import NaverBlog from '../SnsIcon/NaverBlog';
+import LineDiv from './LineDiv';
+import styled, { css } from 'styled-components';
+import KakaoStory from '../SnsIcon/KakaoStory';
+import { Debounce } from '../Debounce';
 //SNS공유하기
 const BtnCss = css`
   border: 0;
@@ -17,21 +17,21 @@ const BtnCss = css`
   cursor: pointer;
 `;
 const Button = styled.button`
-  background: ${(props) => props.color};
+  background: ${props => props.color};
   ${BtnCss}
 `;
 
 const LeftButton = styled.button`
   border-top-left-radius: 5px;
   border-bottom-left-radius: 5px;
-  background: ${(props) => props.color};
+  background: ${props => props.color};
   ${BtnCss}
 `;
 
 const RightButton = styled.button`
   border-top-right-radius: 5px;
   border-bottom-right-radius: 5px;
-  background: ${(props) => props.color};
+  background: ${props => props.color};
   ${BtnCss}
 `;
 const HiddenTextArea = styled.textarea`
@@ -51,31 +51,31 @@ const ToastMsg = styled.p`
 const SnsGroup = () => {
   useEffect(() => {
     if (!window.Kakao.isInitialized())
-      window.Kakao.init("a80efbd3b98c1eb5db9344a5b1289b50");
+      window.Kakao.init('a80efbd3b98c1eb5db9344a5b1289b50');
     //공유할 때 썸네일에 결과만 공유하고 접속은 테스트하는 페이지로 넘기기 위해서(동적으로 썸네일이 안바뀌기 때문에), url에 리다이렉트 함수를 넣어둔 페이지의 url을 넣어준다.
 
     // 카카오톡 공유
 
     window.Kakao.Link.createDefaultButton({
-      container: "#kakao-link-btn",
-      objectType: "feed",
+      container: '#kakao-link-btn',
+      objectType: 'feed',
       content: {
-        title: "로또 맛있게 하는 집 - 로또맛집",
-        description: "실제같은 가상로또부터 천만개가 넘는 로또데이터까지",
-        imageUrl: "https://lottozip.site/Thumbnail1.png",
+        title: '로또 맛있게 하는 집 - 로또맛집',
+        description: '실제같은 가상로또부터 천만개가 넘는 로또데이터까지',
+        imageUrl: 'https://lottozip.ml/Thumbnail1.png',
         imageWidth: 800,
         imageHeight: 400,
         link: {
-          mobileWebUrl: "https://lottozip.site",
-          webUrl: "https://lottozip.site",
+          mobileWebUrl: 'https://lottozip.ml',
+          webUrl: 'https://lottozip.ml',
         },
       },
       buttons: [
         {
-          title: "번호 뽑으러 가기",
+          title: '번호 뽑으러 가기',
           link: {
-            mobileWebUrl: "https://lottozip.site",
-            webUrl: "https://lottozip.site/",
+            mobileWebUrl: 'https://lottozip.ml',
+            webUrl: 'https://lottozip.ml/',
           },
         },
       ],
@@ -88,38 +88,38 @@ const SnsGroup = () => {
 
   var popupY = window.screen.height / 2 - popupHeight / 2;
   var size =
-    "status=no, height=" +
+    'status=no, height=' +
     popupHeight +
-    ", width=" +
+    ', width=' +
     popupWidth +
-    ", left=" +
+    ', left=' +
     popupX +
-    ", top=" +
+    ', top=' +
     popupY;
-  var url = "https://lottozip.site";
+  var url = 'https://lottozip.ml';
   const facebook = Debounce(() => {
     window.open(
-      "http://www.facebook.com/sharer/sharer.php?u=" + url,
-      "name",
-      size
+      'http://www.facebook.com/sharer/sharer.php?u=' + url,
+      'name',
+      size,
     );
   }, 200);
   const kakaoStory = Debounce(() => {
-    window.open("https://story.kakao.com/share?url=" + url, "name", size);
+    window.open('https://story.kakao.com/share?url=' + url, 'name', size);
   }, 200);
   const naver = Debounce(() => {
     window.open(
-      "http://share.naver.com/web/shareView.nhn?url=" + url + "&title=로또맛집",
-      "name",
-      size
+      'http://share.naver.com/web/shareView.nhn?url=' + url + '&title=로또맛집',
+      'name',
+      size,
     );
   }, 200);
   const urlCopy = Debounce(() => {
-    var toast = document.getElementById("toast");
+    var toast = document.getElementById('toast');
     toast.style.opacity = 1;
-    var copyText = document.getElementById("copy");
+    var copyText = document.getElementById('copy');
     copyText.select();
-    document.execCommand("Copy");
+    document.execCommand('Copy');
     copyText.blur();
 
     setTimeout(() => {
@@ -149,7 +149,7 @@ const SnsGroup = () => {
       <HiddenTextArea
         readOnly
         id="copy"
-        value="https://lottozip.site"
+        value="https://lottozip.ml"
       ></HiddenTextArea>
     </div>
   );
